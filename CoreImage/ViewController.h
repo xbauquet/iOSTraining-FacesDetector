38dc22f71ef2e09f797f712f60bedc0cbb058645
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-02-18 13:58:57 +0100

    Initial Commit

---
+++ CoreImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreImage
+//
+//  Created by xavier on 18/02/2016.
+//  Copyright © 2016 xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
